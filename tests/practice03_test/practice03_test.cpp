00db656a8c91ce83b4ca64961481452143a41366
elishadollinger <erdollinger@ualr.edu>
2025-02-07 11:48:02 -0600

    test

--- tests/practice03_test/practice03_test.cpp
+++ tests/practice03_test/practice03_test.cpp
@@ -1,4 +1,8 @@
-# CPSC2376-EDollinger
-Project01
-test
+#include "gtest/gtest.h"
+#include "practice03.cpp"
+class MathUtilsTest : public ::testing::Test {};
 
+TEST_F(MathUtilisTest, SumRangeTest)
+{
+    EXPECT_EQ(MathUtilis::sumRange(1,5), 10);
+}
