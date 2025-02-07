91fdd3f807eab515a8d4449620214db442b6666f
elishadollinger <erdollinger@ualr.edu>
2025-02-07 12:26:50 -0600

    bad

--- tests/practice03_test/practice03_test.cpp
+++
@@ -1,8 +0,0 @@
-#include "gtest/gtest.h"
-#include "practice03.cpp"
-class MathUtilsTest : public ::testing::Test {};
-
-TEST_F(MathUtilisTest, SumRangeTest)
-{
-    EXPECT_EQ(MathUtilis::sumRange(1,5), 10);
-}
