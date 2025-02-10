2aebf42ccc7f407d8f74ec21c23e6b2f434cafba
elishadollinger <erdollinger@ualr.edu>
2025-02-09 20:21:37 -0600

    f

---
+++ practice/practice03/practice03/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  practice03
+//
+//  Created by Eli Dollinger on 2/9/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
