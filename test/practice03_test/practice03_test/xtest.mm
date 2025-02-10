6837d434dd8d0bd381f867b555fd600a7b49fed9
elishadollinger <erdollinger@ualr.edu>
2025-02-09 22:14:31 -0600

    YAYYYY

--- test/practice03_test/practice03_test/xtest.mm
+++ test/practice03_test/practice03_test/xtest.mm
@@ -15,16 +15,6 @@
 
 @implementation xtest
 
-- (void)setUp
-{
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-}
-
 - (void)testSumRange
 {
     int result = MathUtils::sumRange(1,5);
