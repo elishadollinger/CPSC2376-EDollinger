f37e6915b1ce1c3207243927dc8b2a67e61c98d5
elishadollinger <erdollinger@ualr.edu>
2025-04-23 11:26:05 -0500

    add

--- projects/project04/project04/main.cpp
+++ projects/project04/project04/main.cpp
@@ -1,83 +1,121 @@
 #define SDL_MAIN_HANDLED
-
-/*
-    I had to use ChatGPT to change and include this part
-    When i had SDL2.h/SDL.h, nothing was working on XCode
-    However, when I switched to this, my xcode started working
- */
-#include <SDL.h>
 #include <iostream>
- 
-int main() {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+#include <SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2_gfxPrimitives.h>
+
+#include "Game.h"
+#include "Engine.h"
+
+void drawWinScreen(Game& game, Engine& engine)
+{
+    auto status = game.gameStatus();
+    std::string msg;
+    SDL_Color bgColor;
+    SDL_Color textColor;
+    if (status == Game::PLAYER_1_WINS)
     {
-        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
-        return 1;
+        msg = "X WON IN "+std::to_string(game.moveCount()) + " MOVES!!!";
+        bgColor = { 0,0,0,200 };
+        textColor = { 255,255,255,255 };
     }
- 
-    SDL_Window* window = SDL_CreateWindow("Bouncing Rectangle",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
- 
-    
-    int dx = 4, dy = 3;
- 
-    bool running = true;
-    SDL_Event e;
-    bool isSpaceDown = false;
-    int x = 0;
-    
-    while (running)
+
+    else if (status == Game::O_WON)
     {
-        while (SDL_PollEvent(&e))
-        {
-            if (e.type == SDL_QUIT)
-                running = false;
-            if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE)
-                running = false;
-            if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_SPACE)
-                isSpaceDown = true;
-            if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_SPACE)
-                isSpaceDown = false;
-            if(e.type == SDL_MOUSEMOTION)
-            {
-                x = e.motion.x;
-            }
-        }
-        
-        SDL_Rect rect = {x, 100, 50, 50};
-        
-        rect.x += dx;
-        rect.y += dy;
- 
-        if (rect.x < 0 || rect.x + rect.w > 800) dx *= -1;
-        if (rect.y < 0 || rect.y + rect.h > 600) dy *= -1;
- 
-        if(isSpaceDown)
-        {
-            
-        }
-        
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // black background
-        SDL_RenderClear(renderer);
- 
-        if(isSpaceDown)
-        {
-            SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); // red rectangle
-        }
-        else
-        {
-            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // red rectangle
-        }
-        SDL_RenderFillRect(renderer, &rect);
- 
-        SDL_RenderPresent(renderer);
-        SDL_Delay(16); // ~60 FPS
+        msg = "O WON IN " + std::to_string(game.moveCount()) + " MOVES!!!";
+        bgColor = { 255,255,255,200 };
+        textColor = { 0,0,0,255 };
     }
- 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 0;
+    else
+    {
+        msg = "DRAW";
+        bgColor = { 128,128,128,200 };
+        textColor = { 255,255,255,255 };
+    }
+    engine.drawRectangle(350, 350, 700, 700, bgColor);
+    engine.drawText(msg, 350, 350, textColor);
+    engine.drawText("Press space to play again.", 350, 650, textColor);
 }
 
+int main() {
+
+    Engine engine;
+   /* while (true)
+    {
+        engine.clear();
+        engine.drawCircle(300, 300, 100);
+        engine.drawRectangle(300, 300);
+        engine.drawText("Hello, SDL2!", 300, 300);
+        engine.flip();
+    }*/
+
+
+
+
+
+
+    Game game;
+    bool running = true;
+    SDL_Event event;
+    int row = 0;
+    int col = 0;
+
+    while (running)
+    {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+            else if (event.type == SDL_KEYDOWN) {
+                if (event.key.keysym.sym == SDLK_ESCAPE) {
+                    running = false;
+                }
+                if (event.key.keysym.sym == SDLK_SPACE)
+                {
+
+                    if (game.getStatus() != Game::ONGOING) game=Game();
+                }
+            }
+
+            else if (event.type == SDL_MOUSEBUTTONDOWN)
+            {
+                int col = event.button.x / 100;
+                int row = event.button.y / 100;
+                if (game.getStatus() == Game::ONGOING)
+                {
+                    game.play(row, col);
+                    engine.playSound();
+                }
+                
+                //x = event.motion.x;
+            }
+
+            else if (event.type == SDL_MOUSEMOTION)
+            {
+                col = event.motion.x / 100;
+                row = event.motion.y / 100;
+                if (game.getStatus() != Game::ONGOING)
+                {
+                    col = -1;
+                    row = -1;
+                }
+                //x = event.motion.x;
+            }
+        }
+
+
+        
+
+        engine.clear();
+        game.draw(&engine,row,col);
+
+        if (game.getStatus() != Game::ONGOING) drawWinScreen(game, engine);
+
+
+        engine.flip();
+    }
+        
+
+
+    return 0;
+}
